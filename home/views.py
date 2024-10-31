c67c65bcdb1089984ef1952b76f8696bc7a90c11
Samarpan Mathangi <samarpanraaz@gmail.com>
2024-10-31 05:51:15 +0000

    added about and contact page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
 
 # Create your views here.
 def index(request):
-    """ A view to return the index page """
+    """ A view to return the home page """
     
     return render(request, 'home/index.html')
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def contact(request):
+    if request.method == "POST":
+        # Process form data here, e.g., save it or send an email
+        messages.success(request, "Your message has been sent!")
+        return redirect('contact')
+    return render(request, 'home/contact.html')
