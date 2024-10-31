275a687a9f2a11d9df7811bf0f0e221465a5c408
Samarpan Mathangi <samarpanraaz@gmail.com>
2024-10-31 06:01:57 +0000

    setup lessons page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Lesson(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.CharField(max_length=50, choices=[('EDT', 'EDT Lessons'), ('Pre-Test', 'Pre-Test Lessons'), ('Car Hire', 'Car Hire for Tests')])
+
+    def __str__(self):
+        return self.title
